e8191dae9db44b08866fb6e38200c3696227e021
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-11-30 03:14:49 -0700

    actualización package.json

--- src/tests/prueba.test.js
+++ src/tests/prueba.test.js
@@ -2,4 +2,8 @@ describe('grupo de pruebas test', () => {
     test('suma 1+2', () => {
         expect(1 + 2).toBe(3);
     });
+    
+    test('resta 1-2', () => {
+        expect(1 - 2).toBe(-1);
+    });
 });
